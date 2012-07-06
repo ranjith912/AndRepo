6c3c03a0f31aafbeb8d3c7ef8b7863771b4a2f31
Alagappan Karuppaiah <karuppaiah.al@gmail.com>
2012-07-06 12:53:55 +0800

    Added a new View for list view and list item view
    Added a gradient display as background on all pages
    Added colors.xml

--- src/com/eshiah/base/AppActivity.java
+++ src/com/eshiah/base/AppActivity.java
@@ -2,12 +2,20 @@ package com.eshiah.base;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.eshiah.adapter.RuleAdapter;
 
 public class AppActivity extends Activity {
+	RuleAdapter ruleAdapter;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ListView ruleListView=(ListView)findViewById(R.id.rule_list);
+        ruleAdapter = new RuleAdapter();
+        ruleListView.setAdapter(ruleAdapter);
     }
 }
