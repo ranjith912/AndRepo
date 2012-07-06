5623e1341d6944588a5382c538a840fb958eef0a
Alagappan Karuppaiah <karuppaiah.al@gmail.com>
2012-07-06 14:55:09 +0800

    added lot of 9 patch image
    added  menu in rulelist page
    added new activity for new rule add page

---
+++ src/com/eshiah/base/AddRuleActivity.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.eshiah.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author akaruppaiah
+ *
+ */
+public class AddRuleActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addrule);
+		}
+
+}
--- src/com/eshiah/base/AppActivity.java
+++ src/com/eshiah/base/AppActivity.java
@@ -1,7 +1,11 @@
 package com.eshiah.base;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.eshiah.adapter.RuleAdapter;
@@ -18,4 +22,21 @@ public class AppActivity extends Activity {
         ruleAdapter = new RuleAdapter();
         ruleListView.setAdapter(ruleAdapter);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+    	super.onCreateOptionsMenu(m);
+    	MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.rulelistmenu,m);
+        return true;
+    }
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    	if (item.getItemId() == R.id.add_rule_menu_item) {
+    		Intent intent = new Intent(this, AddRuleActivity.class);
+    		startActivity(intent);
+    		return true;
+    	}
+    	return super.onOptionsItemSelected(item);
+    }
+    
 }
